0e59d4f8941026d32154d10fc9d237c0fcd83d24
Tsukimi <hrj1125@qq.com>
2023-07-16 13:02:14 +0800

    fix: line

--- GbitsGamejam/Assets/PipelineSetting/RenderFeatures/LineRenderPass.cs
+++ GbitsGamejam/Assets/PipelineSetting/RenderFeatures/LineRenderPass.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Mathematics;
+using System.IO;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
@@ -22,6 +22,48 @@ public class LineRenderPass : ScriptableRenderPass
     public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
     {
         ConfigureInput(ScriptableRenderPassInput.Depth | ScriptableRenderPassInput.Normal | ScriptableRenderPassInput.Color);
+    }
+
+    Texture2D _noiseTexture;
+    RenderTexture _trashFrame1;
+    RenderTexture _trashFrame2;
+
+    void UpdateNoiseTexture(int frame, int noiseTextureWidth, int noiseTextureHeight, float stripLength)
+    {
+        int frameCount = Time.frameCount;
+        if (frameCount % frame != 0)
+        {
+            return;
+        }
+
+        _noiseTexture = new Texture2D(noiseTextureWidth, noiseTextureHeight, TextureFormat.ARGB32, false);
+        _noiseTexture.wrapMode = TextureWrapMode.Clamp;
+        _noiseTexture.filterMode = FilterMode.Point;
+
+        _trashFrame1 = new RenderTexture(Screen.width, Screen.height, 0);
+        _trashFrame2 = new RenderTexture(Screen.width, Screen.height, 0);
+        _trashFrame1.hideFlags = HideFlags.DontSave;
+        _trashFrame2.hideFlags = HideFlags.DontSave;
+
+        Color32 color = new Color(Random.value, Random.value, Random.value, Random.value);
+
+        for (int y = 0; y < _noiseTexture.height; y++)
+        {
+            for (int x = 0; x < _noiseTexture.width; x++)
+            {
+                //随机值若大于给定strip随机阈值，重新随机颜色
+                if (UnityEngine.Random.value > stripLength)
+                {
+                    color = new Color(Random.value, Random.value, Random.value, Random.value);
+                }
+                //设置贴图像素值
+                _noiseTexture.SetPixel(x, y, color);
+            }
+        }
+
+        _noiseTexture.Apply();
+
+        var bytes = _noiseTexture.EncodeToPNG();
     }
 
     public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
@@ -49,16 +91,35 @@ public class LineRenderPass : ScriptableRenderPass
             cmd.GetTemporaryRT(originID, descriptor1);
             cmd.GetTemporaryRT(targetID, descriptor2);
 
+            int kernel;
+
+            if (settings.lineMode == LineRendererFeature.LineSettings.LineMode.Vertical)
+            {
+                kernel = computeShader.FindKernel("Line");
+            }
+            else
+            {
+                kernel = computeShader.FindKernel("LineHorizontal");
+            }
+
+            UpdateNoiseTexture(settings.frequncy, settings.noiseTextureWidth, settings.noiseTextureHeight, settings.stripeLength);
+
             cmd.SetComputeFloatParam(settings.computeShader, "_LineIntensity", settings.LineIntensity);
             cmd.SetComputeFloatParam(settings.computeShader, "_VignetteIntensity", settings.VignetteIntensity);
             cmd.SetComputeFloatParam(settings.computeShader, "_VignetteSmoothness", settings.VignetteSmoothness);
             cmd.SetComputeFloatParam(settings.computeShader, "_VignetteRoundness", settings.VignetteRoundness);
-            cmd.SetComputeIntParam(settings.computeShader, "_UseGray", settings.useGray ? 1 : 0);
-            cmd.SetComputeVectorParam(settings.computeShader, "_BufferSize", new float4(width, height, 1.0f / width, 1.0f / height));
-            cmd.SetComputeTextureParam(settings.computeShader, 0, "_MainTex", originID);
-            cmd.SetComputeTextureParam(settings.computeShader, 0, "Result", targetID);
 
-            cmd.DispatchCompute(settings.computeShader, 0, width / 8, height / 8, 1);
+            cmd.SetComputeTextureParam(settings.computeShader, kernel, "_NoiseTex", _noiseTexture);
+            cmd.SetComputeFloatParam(settings.computeShader, "_HorizontalIntensity", settings.HorizontalIndensity);
+            cmd.SetComputeFloatParam(settings.computeShader, "_StripColorAdjustIndensity", settings.StripColorAdjustIndensity);
+            cmd.SetComputeFloatParam(settings.computeShader, "_StripColorAdjustColor", settings.StripColorAdjustColor);
+
+            cmd.SetComputeIntParam(settings.computeShader, "_UseGray", settings.useGray ? 1 : 0);
+            cmd.SetComputeVectorParam(settings.computeShader, "_BufferSize", new Vector4(width, height, 1.0f / width, 1.0f / height));
+            cmd.SetComputeTextureParam(settings.computeShader, kernel, "_MainTex", originID);
+            cmd.SetComputeTextureParam(settings.computeShader, kernel, "Result", targetID);
+
+            cmd.DispatchCompute(settings.computeShader, kernel, width / 8, height / 8, 1);
 
             cmd.Blit(targetID, renderingData.cameraData.renderer.cameraColorTarget);
 
--- GbitsGamejam/Assets/PipelineSetting/RenderFeatures/LineRendererFeature.cs
+++ GbitsGamejam/Assets/PipelineSetting/RenderFeatures/LineRendererFeature.cs
@@ -10,9 +10,34 @@ public class LineRendererFeature : ScriptableRendererFeature
     public class LineSettings
     {
         public ComputeShader computeShader;
+        public enum LineMode
+        {
+            Vertical,
+            Horizontal,
+        }
+        public LineMode lineMode = LineMode.Vertical;
         public bool useGray = true;
+        [Header("Vertical Line")]
         [Range(0, 1)]
         public float LineIntensity = 0.15f;
+        [Header("Horizontal Line")]
+        [Range(1, 10)]
+        public int frequncy = 8;
+
+        [Range(16, 256)]
+        public int noiseTextureWidth = 32;
+        [Range(16, 256)]
+        public int noiseTextureHeight = 256;
+        [Range(0, 1)]
+        public float stripeLength = 0.85f;
+
+        public float HorizontalIndensity = 0.15f;
+        [Range(0, 1)]
+        public float StripColorAdjustIndensity = 0.15f;
+        [Range(0, 1)]
+        public float StripColorAdjustColor = 0.15f;
+        public Texture2D NoiseTexture;
+
         [Header("Vignette")]
         [Range(0, 3)]
         public float VignetteIntensity = 1.0f;
