0f1bbac122361d47fb5fc6ff93a00abe91748b94
foral132 <1911823191@qq.com>
2023-07-16 10:39:50 +0800

    修bug

--- GbitsGamejam/Assets/Scripts/Player/PlayerHJ_D.cs
+++ GbitsGamejam/Assets/Scripts/Player/PlayerHJ_D.cs
@@ -13,6 +13,7 @@ public class PlayerHJ_D : PlayerHJ
             var Body = Instantiate(ResoucesManager.Instance.Resouces["PlayerBody"], this.transform.position, Quaternion.identity);
             Body.transform.rotation = Quaternion.Euler(0, 0, 90f);
             GameMode.Instance.playerDeathSection = GameMode.Instance.timeSectionManager.NowTimeSection;
+
             GameMode.Instance.RespawnPlayer(false);
             Destroy(this.gameObject);
         }
